047f924b461d2257f67ae4fe14f037e39b0436a1
FABIOLAGOMEZ <40718817+FABIOLAGOMEZ@users.noreply.github.com>
2018-07-30 14:21:58 -0500

    Update Creacion_Tablas.sql
    Creación en BD Postgres

--- Creacion_Tablas.sql
+++ Creacion_Tablas.sql
@@ -36,6 +36,64 @@ CREATE TABLE Policia (
 	id_cai INT FOREIGN KEY REFERENCES Cai(id_cai)
 );
 
+----Postgres---
+CREATE TABLE Usuarios (
+	id_usuario INT NOT NULL PRIMARY KEY,
+	nombre varchar(100),
+	apellido varchar(100) ,
+	genero varchar(100) ,
+	direccion varchar(100),
+	correo varchar(100) ,
+	celular INT
+);
+
+CREATE TABLE Estado (
+	id_estado SERIAL PRIMARY KEY,
+	nombre varchar(100)
+);
+
+CREATE TABLE Localidad (
+	id_localidad SERIAL PRIMARY KEY,
+	nombre varchar(100)
+);
+
+CREATE TABLE Cai (
+	id_cai SERIAL PRIMARY KEY,
+	nombre varchar(100),
+	direccion varchar(100),
+	telefono INT,
+	longitud decimal(4,1),
+	latitud decimal(4,1),
+	id_localidad INT REFERENCES Localidad(id_localidad)
+);
+
+CREATE TABLE Policia (
+	id_placa SERIAL PRIMARY KEY,
+	nombre varchar(100),
+	apellido varchar(100),
+	telefono INT,
+	id_cai INT REFERENCES Cai(id_cai)
+);
+
+
+CREATE TABLE Alerta (
+	id_alerta SERIAL PRIMARY KEY,
+	id_usuario INT REFERENCES Usuarios(id_usuario),
+	id_estado INT REFERENCES Estado(id_estado),
+	id_placa INT REFERENCES Policia(id_placa),
+	longitud decimal(4,1),
+	latitud decimal(4,1)
+);
+
+
+CREATE TABLE Servicio (
+	id_servicio SERIAL PRIMARY KEY,
+	id_usuario INT REFERENCES Usuarios(id_usuario),
+	id_placa INT REFERENCES Policia(id_placa),
+	fecha date,
+	hora time
+);
+
 
 CREATE TABLE Alerta (
 	id_alerta INT IDENTITY(1,1) PRIMARY KEY,
